8789f400ae54d05077e674ff25b419680677d42b
fxcx <87233669+fxcx@users.noreply.github.com>
2024-07-02 18:57:04 -0300

    test Elisya 🦊

---
+++ elysiajs-mongodb-primsa-crud-operations-example-main/README.md
@@ -0,0 +1,20 @@
+# Elysia with Bun runtime
+
+## Getting Started
+To install packages:
+```bash
+bun install
+```
+
+update mongodb url in .env
+```bash
+DATABASE_URL="mongodb://localhost:27017/db"
+```
+
+## Development
+To start the development server run:
+```bash
+bun run dev
+```
+
+Open http://localhost:3000/ with your browser to see the result.
---
+++ elysiajs-mongodb-primsa-crud-operations-example-main/src/index.ts
@@ -0,0 +1,58 @@
+import { Elysia } from "elysia";
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+const app = new Elysia()
+  .get("/users", async () => {
+    const users = await db.user.findMany();
+    return users;
+  })
+  .post("/users", async (req) => {
+    const { name, email }: any = req.body;
+    const user = await db.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    return user;
+  })
+  .put("/users/:id", async (req) => {
+    const { id } = req.params;
+    const { name, email }: any = req.body;
+    const user = await db.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name,
+        email,
+      },
+    });
+    return user;
+  })
+  .delete("/users/:id", async (req) => {
+    const { id } = req.params;
+    const user = await db.user.delete({
+      where: {
+        id: id,
+      },
+    });
+    return user;
+  })
+  .get("/users/:id", async (req) => {
+    const { id } = req.params;
+    const user = await db.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+    return user;
+  })
+  .get("/", () => "Hello World! from Elysia")
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
